032ab6a8298afcafc177628efe3528058ebcb4aa
mjorrico <jordanenrico214@gmail.com>
2023-03-26 09:19:08 +0200

    prefetching

--- 002_sharedmem.cu
+++ 002_sharedmem.cu
@@ -14,6 +14,7 @@ __global__ void add_vector(float *d_a, float *d_b, float *d_c, float p, float N)
 int main()
 {
   const int N = 10;
+  int id = cudaGetDevice(&id);
 
   float *a, *b, *c;
   cudaMallocManaged(&a, N * sizeof(float));
@@ -26,15 +27,22 @@ int main()
     b[i] = (float)rand() / RAND_MAX;
   }
 
+  // Uncomment cudaMemPrefetchAsync to prefetch from host to device
+  cudaMemPrefetchAsync(a, N * sizeof(float), id);
+  cudaMemPrefetchAsync(b, N * sizeof(float), id);
+
   const int block_size = 64;
   const int grid_size = (N + block_size - 1) / block_size;
-  std::cout << "Grid size: " << grid_size << ". Block size: " << block_size << "." << std::endl;
+  std::cout << "Device ID: " << id << ". Grid size: " << grid_size << ". Block size: " << block_size << "." << std::endl;
   add_vector<<<grid_size, block_size>>>(a, b, c, 1.9f, N);
 
   // Wait for all previous operations before using values
   // We need this because we don't get the implicit synchronization of
   // cudaMemcpy like in the original example
   cudaDeviceSynchronize();
+
+  // Uncomment cudaMemPrefetchAsync to prefetch from host to device
+  cudaMemPrefetchAsync(c, N * sizeof(float), cudaCpuDeviceId);
 
   for (std::size_t i = 0; i < N; i++)
   {
